166139b95e72328d24520aacd6b710c5a7ac68da
h-abe0181 <hiroki.abe@excd.jp>
2022-06-15 12:13:25 +0900

    β版、動かすこと自体は問題ないがコメントアウト前

--- src/main/java/jp/excd/servlet/S00003.java
+++ src/main/java/jp/excd/servlet/S00003.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -25,7 +27,7 @@ public class S00003 extends HttpServlet{
 		//DB接続
 
 		try {
-			con = MySQLSetting.getConnection("meloko","meloko","exceed","JST");
+			con = MySQLSetting.getConnection("meloko","meloko","exceed","Asia/Tokyo");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -59,32 +61,33 @@ public class S00003 extends HttpServlet{
 
 	private void mainSongSearch(HttpServletRequest request, HttpServletResponse response, Connection con, PreparedStatement pstmt,ResultSet rs) throws ServletException, IOException, SQLException {
 
-		// 接続URL受け取り
-		String requestUrl = request.getRequestURL().toString();
+		//受け取ったURIをrequestURI()メソッドで取得し、requestUri変数で受け取る。
+		String requestUri= request.getRequestURI();
 
-		// (1) 接続URLが「/ja/S00005/searh」以外の場合は、404.jspへフォワーディングする。
-		if ("/web/ja/S00005/search".equals(requestUrl)) {
+		//（1）spilit()を使い、取得したuriを分割。。
+		String[] uri = requestUri.split("/");
+
+		//(2)接続URLが「/ja/S00003/*」以外の場合は、404.jspへフォワーディングする
+		String urlCheck = "/" + uri[1] + "/" + uri[2] + "/" +uri[3] + "/";
+		if ("/web/ja/S00003/".equals(urlCheck)) {
 		} else {
 			getServletConfig().getServletContext().getRequestDispatcher("/jsp/404.jsp").forward(request, response);
 		}
-		//（2）受け取ったURIをrequestURI()メソッドで取得し、requestUri変数で受け取る。
-		String requestUri= request.getRequestURI();
 
-		//（3）spilit()を使い、取得したuriを分割。url内の曲id/song_idを取得し、変数idに代入する。
-		String[] uri = requestUri.split("/");
+		//(3)url内の曲id/song_idを取得し、変数idに代入する。
+
 		String id = uri[4];
 
 
 		//(4)SQL文の設定
-		String SongSql = "select"
+		// 曲＋作曲者
+		String songSql = "select"
 				+ "    song.title"
 				+ "    , song.composer_id"
-				+ "    , song.rating_total"
-				+ "    , song.rating_average"
 				+ "    , song.total_listen_count"
 				+ "    , song.release_datetime"
 				+ "    , song.last_update_datetime"
-				+ "    , sonf.message"
+				+ "    , song.message"
 				+ "    , song.key"
 				+ "    , song.score_type"
 				+ "    , song.bpm"
@@ -100,116 +103,186 @@ public class S00003 extends HttpServlet{
 				+ "    left join composer "
 				+ "        on song.composer_id = composer.id "
 				+ "where"
-				+ "    id = ?";
+				+ "    song.id = ?";
 
+		// コメント＋評価
+		String commentSql = "select "
+				+ "comment.id "
+				+ ", comment.song_id"
+				+ ", comment.sequence"
+				+ ", comment.composer_id"
+				+ ", comment.comment"
+				+ ", comment.type"
+				+ ", comment.to_comment_id"
+				+ ", comment.write_datetime"
+				+ ", rating.rating"
+				+ ", composer.unique_code"
+				+ ", composer.nickname "
+				+ "from "
+				+ "((rating left join comment "
+				+ "on rating.composer_id = comment.composer_id "
+				+ "and rating.song_id = comment.song_id) "
+				+ "left join composer "
+				+ "on rating.composer_id = composer.id) "
+				+ "where comment.song_id = ? "
+				+ "and comment.comment is not null "
+				+ "and comment.type = ?"
+				+ "order by comment.sequence asc";
 
-		String CommentSql ="select"
-				+ "    comment.sequence"
-				+ "    , comment.composer_id"
-				+ "    , comment.comment"
-				+ "    , comment.type"
-				+ "    , comment.to_comment_id"
-				+ "    , comment.write_datetime"
-				+ "    , rating.rating"
-				+ "    , composer.unique_code"
-				+ "    , composer.nickname "
-				+ "from"
-				+ "    ( "
-				+ "        ( "
-				+ "            comment "
-				+ "                left join rating "
-				+ "                    on comment.song_id = rating.song_id"
-				+ "        ) "
-				+ "            left join composer "
-				+ "                on comment.composer_id = composer.id "
-				+ "        where"
-				+ "            comment.song_id = ?"
-				+ "   order by sequence asc";
+		String ratingSql = "select "
+				+ "sum(rating) as rating_total "
+				+ ", avg(rating) as rating_average "
+				+ "from rating  "
+				+ "where song_id = ?"; 
 
-		//（5）SongSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
-		pstmt = con.prepareStatement(SongSql);
+		//（5）songSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
+		pstmt = con.prepareStatement(songSql);
 
 		//(6)Sqlの？に（3）で値を代入したidをセットして実行する
 		pstmt.setString(1, id);
+
 
 		//（7）executeQueryを実行し、結果の「ResultSet」を得る。
 		rs = pstmt.executeQuery();
 
 		//(8)クラスDataBox（他シートに記載）、クラスCommonFormat（共通設計に記載）のインスタンスを作成。
-		DataBox db = new DataBox();
-		List<DataBox> SongList = new ArrayList<DataBox>();
+		SongBean songBean = new SongBean();
+		boolean noRecordflg = false;
+
 
 		//(8)while文を通してSongListに追加。
 		while(rs.next()) {
-			db.setTitle(rs.getString("title"));
-			db.setRatingTotal(CommonUtils.valueformat(rs.getLong("composer_id")));
-			db.setRatingAverage(CommonUtils.averageformat(rs.getDouble("rating_average")));
-			db.setTotalListenCount(CommonUtils.valueformat(rs.getLong("total_listen_count")));
-			db.setReleaseDatetime(CommonUtils.epoch(rs.getDouble("release_datetime")));
-			db.setLastUpdateDatetime(CommonUtils.epoch(rs.getDouble("last_update_datetime")));
-			db.setMessage(rs.getString("message"));
-			db.setKey(rs.getString("key"));
-			db.setScoreType(rs.getString("score_type"));
-			db.setBpm(CommonUtils.ratingformat(rs.getByte("bpm")));
-			db.setImageFileName(rs.getString("image_file_name"));
-			db.setImageFileHeight(rs.getInt("image_file_height"));
-			db.setImageFileWidth(rs.getInt("image_file_width"));
-			db.setOtherLinkUrl(rs.getString("other_link_url"));
-			db.setOtherLinkDescription(rs.getString("other_link_description"));
-			db.setNickname1(rs.getString("nickname"));
-			db.setUniqueCode1(rs.getString("unique_code"));
-
-			SongList.add(db);
+			songBean.setTitle(rs.getString("title"));
+			songBean.setTotalListenCount(CommonUtils.valueformat(rs.getLong("total_listen_count")));
+			songBean.setRatingTotal(CommonUtils.valueformat(rs.getLong("composer_id")));
+			songBean.setReleaseDatetime(CommonUtils.epoch(rs.getDouble("release_datetime")));
+			songBean.setLastUpdateDatetime(CommonUtils.epoch(rs.getDouble("last_update_datetime")));
+			songBean.setMessage(rs.getString("message"));
+			songBean.setKey(CommonUtils.keyName(rs.getString("key")));
+			songBean.setScoreType(CommonUtils.scoreChange(rs.getString("score_type")));
+			songBean.setBpm(rs.getString("bpm"));
+			songBean.setImageFileName(rs.getString("image_file_name"));
+			songBean.setImageFileHeight(rs.getInt("image_file_height"));
+			songBean.setImageFileWidth(rs.getInt("image_file_width"));
+			songBean.setOtherLinkUrl(rs.getString("other_link_url"));
+			songBean.setOtherLinkDescription(rs.getString("other_link_description"));
+			songBean.setNickname1(rs.getString("nickname"));
+			songBean.setUniqueCode1(rs.getString("unique_code"));
+			noRecordflg = true;
 		}
 
-		//（9）SongListに追加したデータをsetAttributeする。この時、listの中身に何も入っていない（idに該当するデータが何もなかった）場合、404.jspにフォワーディングし、これ以降の処理は一切行わない。
+		//（9）dbに追加したデータをsetAttributeする。この時、listの中身に何も入っていない（idに該当するデータが何もなかった）場合、404.jspにフォワーディングし、これ以降の処理は一切行わない。
 
-		if(SongList.size() <= 0) {
+		if(!noRecordflg) {
 			getServletConfig().getServletContext().getRequestDispatcher("/jsp/404.jsp").forward(request, response);
 		}else{
-			request.setAttribute("SongList", SongList);
+			request.setAttribute("songBean", songBean);
 		}
 
-		//（10）SongSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
-		pstmt = con.prepareStatement(CommentSql);
+		//（10）CommentSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
+		pstmt = con.prepareStatement(commentSql);
 
 		//(11)Sqlの？に（3）で値を代入したidをセットして実行する
 		pstmt.setString(1, id);
+		pstmt.setString(2, "0");
 
 		//（12）executeQueryを実行し、結果の「ResultSet」を得る。
 		rs = pstmt.executeQuery();
 
-		//(13)クラスDataBox（他シートに記載）、クラスCommonFormat（共通設計に記載）のインスタンスを作成。
-		DataBox db2 = new DataBox();
-		List<DataBox>CommentList = new ArrayList<DataBox>();		
+		//(13)linkedhashmapを宣言する。
+		LinkedHashMap<String,ParentBean> parentMap = new LinkedHashMap<String, ParentBean>();
 
-		//(13)while文を通してCommentListに追加。
+		//(13)while文を通してlinkedhashmapにコメントタイプ0のデータを入力する。
 		while(rs.next()) {
-
-			db2.setComment(rs.getString("comment"));
-			db2.setSequence(rs.getInt("sequence"));
-			db2.setComposerId(CommonUtils.idformat(rs.getLong("composer_id")));
-			db2.setType(rs.getString("type"));
-			db2.setToCommentId(CommonUtils.idformat(rs.getLong("to_comment_id")));
-			db2.setWriteDatetime(CommonUtils.epoch(rs.getDouble("write_datetime")));
-			db2.setRating(CommonUtils.ratingformat(rs.getByte("rating")));
-			db2.setNickname2(rs.getString("nickname"));
-			db2.setUniqueCode2(rs.getString("unique_code2"));
-
-			CommentList.add(db2);
+			ParentBean parBean = new ParentBean();
+			parBean.setComment(rs.getString("comment"));
+			parBean.setComposerId(CommonUtils.idformat(rs.getLong("composer_id")));
+			parBean.setType(rs.getString("type"));
+			parBean.setWriteDatetime(CommonUtils.epoch(rs.getDouble("write_datetime")));
+			parBean.setRating(CommonUtils.starformat(CommonUtils.ratingformat(rs.getByte("rating"))));
+			parBean.setNickname2(rs.getString("nickname"));
+			parBean.setUniqueCode2(rs.getString("unique_code"));
+			String commentId = rs.getString("id");
+			parentMap.put(commentId , parBean);
 		}
-		
-		//（14）以上でCommentListに追加したデータをsetAttributeする。
-		request.setAttribute("CommentList", CommentList);
 
+		//(14)続いてコメントタイプ1（返信コメントのデータ）をSql文を使って取得する。手順は（10）～（12）と同様だが、二つ目の?には1を充てる。
+		pstmt = con.prepareStatement(commentSql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, "1");
+		rs = pstmt.executeQuery();
+
+		//(15)(13)でlinkedhashmapに追加したデータの中で、今回取得したtoCommentIdの値が等しいキーがある場合、そのデータのlistにcommentBeanをaddする。
+		while(rs.next()) {
+			CommentBean comBean = new CommentBean();
+			comBean.setComment(rs.getString("comment"));
+			comBean.setComposerId(CommonUtils.idformat(rs.getLong("composer_id")));
+			comBean.setType(rs.getString("type"));
+			comBean.setWriteDatetime(CommonUtils.epoch(rs.getDouble("write_datetime")));
+			comBean.setRating(CommonUtils.starformat(CommonUtils.ratingformat(rs.getByte("rating"))));
+			comBean.setNickname2(rs.getString("nickname"));
+			comBean.setUniqueCode2(rs.getString("unique_code"));
+			String toCommentId =CommonUtils.idformat(rs.getLong("to_comment_id"));
+			ParentBean data = parentMap.get(toCommentId);
+			data.getreplyComment().add(comBean);
+		}
+
+		List<CommentBean> commentList = new ArrayList<CommentBean>();
+		for (Iterator<ParentBean> itr = parentMap.values().iterator(); itr.hasNext();) {
+			ParentBean pb = itr.next();
+			CommentBean cb = new CommentBean();
+			cb.setComment(pb.getComment());
+			cb.setSequence(pb.getSequence());
+			cb.setType(pb.getType());
+			cb.setToCommentId(pb.getToCommentId());
+			cb.setWriteDatetime(pb.getWriteDatetime());
+			cb.setNickname2(pb.getNickname2());
+			cb.setUniqueCode2(pb.getUniqueCode2());
+			cb.setRating(pb.getRating());
+			commentList.add(cb);
+			if (0 < pb.getreplyComment().size()) {
+				for( CommentBean replycb : pb.getreplyComment() ) {
+					commentList.add(replycb);
+				}
+			}
+		}
+
+		//（14）以上でCommentListに追加したデータをsetAttributeする。
+		request.setAttribute("commentList", commentList);
+		
 		//（15）jsp画像が押下された際にmelokoを起動するURLを作成する。
 		String melokoUrl = "meloko://?song_id=".concat(id);
-		
+
 		//（16）（15）で設定したmelokoUrlをsetAttributeする。
 		request.setAttribute("melokoUrl", melokoUrl);
-		
-		//（17）S00003にフォワーディングする。
-		getServletConfig().getServletContext().getRequestDispatcher("/jsp/output.jsp" ).forward( request, response );
+
+		//（17）RatingSqlを使い、引数で渡されたcon.prepareStatement()メソッドを実行する。
+		pstmt = con.prepareStatement(ratingSql);
+
+		//(18)Sqlの？に（3）で値を代入したidをセットして実行する
+		pstmt.setString(1, id);
+
+		//（19）executeQueryを実行し、結果の「ResultSet」を得る。
+		rs = pstmt.executeQuery();
+
+		//(20)RatingSqlでのデータを受け取る変数を宣言する。
+		String ratingAverage = null;
+		String ratingTotal= null;
+
+
+		//(21)while文を通してそれぞれの値を（20）で宣言した変数にに追加。
+		while(rs.next()) {
+			ratingAverage = CommonUtils.averageformat(rs.getDouble("rating_average"));
+			ratingTotal = CommonUtils.valueformat(rs.getLong("rating_total"));
+		}
+
+		//（22）取得した値（変数）をsetAttributeする。
+		request.setAttribute("rating_average", ratingAverage);
+		request.setAttribute("rating_total", ratingTotal);
+
+		//（23）S00003にフォワーディングする。
+		getServletConfig().getServletContext().getRequestDispatcher("/jsp/S00003.jsp" ).forward( request, response );
+
 
 	}
 }
