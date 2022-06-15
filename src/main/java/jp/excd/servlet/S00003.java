6932cd19fb4d8beb25daa71fbe148d8fb29b1a88
h-abe0181 <hiroki.abe@excd.jp>
2022-06-15 12:57:47 +0900

    コメントの追加と軽微な修正（不必要な処理の削除）

--- src/main/java/jp/excd/servlet/S00003.java
+++ src/main/java/jp/excd/servlet/S00003.java
@@ -54,30 +54,28 @@ public class S00003 extends HttpServlet{
 			} catch (SQLException e) {
 				throw new ServletException();
 			}
-
 		}
-
 	}
-
+	//呼び出される内部メソッド
 	private void mainSongSearch(HttpServletRequest request, HttpServletResponse response, Connection con, PreparedStatement pstmt,ResultSet rs) throws ServletException, IOException, SQLException {
 
 		//受け取ったURIをrequestURI()メソッドで取得し、requestUri変数で受け取る。
 		String requestUri= request.getRequestURI();
 
-		//（1）spilit()を使い、取得したuriを分割。。
+		//(1)spilit()を使い、取得したuriを分割。。
 		String[] uri = requestUri.split("/");
 
 		//(2)接続URLが「/ja/S00003/*」以外の場合は、404.jspへフォワーディングする
 		String urlCheck = "/" + uri[1] + "/" + uri[2] + "/" +uri[3] + "/";
+		//"/" + uri[1] + "/" + uri[2] + "/" +uri[3] + "/"が「/ja/S00003/」となっていれば処理続行。
+
 		if ("/web/ja/S00003/".equals(urlCheck)) {
 		} else {
 			getServletConfig().getServletContext().getRequestDispatcher("/jsp/404.jsp").forward(request, response);
 		}
 
 		//(3)url内の曲id/song_idを取得し、変数idに代入する。
-
 		String id = uri[4];
-
 
 		//(4)SQL文の設定
 		// 曲＋作曲者
@@ -129,26 +127,25 @@ public class S00003 extends HttpServlet{
 				+ "and comment.type = ?"
 				+ "order by comment.sequence asc";
 
+		//総感動指数+平均感動指数とその計算
 		String ratingSql = "select "
 				+ "sum(rating) as rating_total "
 				+ ", avg(rating) as rating_average "
 				+ "from rating  "
 				+ "where song_id = ?"; 
 
-		//（5）songSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
+		//(5)songSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
 		pstmt = con.prepareStatement(songSql);
 
 		//(6)Sqlの？に（3）で値を代入したidをセットして実行する
 		pstmt.setString(1, id);
 
-
-		//（7）executeQueryを実行し、結果の「ResultSet」を得る。
+		//(7)executeQueryを実行し、結果の「ResultSet」を得る。
 		rs = pstmt.executeQuery();
 
 		//(8)クラスDataBox（他シートに記載）、クラスCommonFormat（共通設計に記載）のインスタンスを作成。
 		SongBean songBean = new SongBean();
 		boolean noRecordflg = false;
-
 
 		//(8)while文を通してSongListに追加。
 		while(rs.next()) {
@@ -171,7 +168,10 @@ public class S00003 extends HttpServlet{
 			noRecordflg = true;
 		}
 
-		//（9）dbに追加したデータをsetAttributeする。この時、listの中身に何も入っていない（idに該当するデータが何もなかった）場合、404.jspにフォワーディングし、これ以降の処理は一切行わない。
+		//(9）dbに追加したデータをsetAttributeする。
+		//この時、listの中身に何も入っていない（idに該当するデータが何もなかった）場合、
+		//404.jspにフォワーディングし、これ以降の処理は一切行わない。
+		//この判定はnoRecordflgがfalseかどうかで決める。
 
 		if(!noRecordflg) {
 			getServletConfig().getServletContext().getRequestDispatcher("/jsp/404.jsp").forward(request, response);
@@ -179,20 +179,21 @@ public class S00003 extends HttpServlet{
 			request.setAttribute("songBean", songBean);
 		}
 
-		//（10）CommentSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
+		//(10)CommentSqlを使い、引数で渡されたconn.prepareStatement()メソッドを実行する。
 		pstmt = con.prepareStatement(commentSql);
 
 		//(11)Sqlの？に（3）で値を代入したidをセットして実行する
 		pstmt.setString(1, id);
 		pstmt.setString(2, "0");
 
-		//（12）executeQueryを実行し、結果の「ResultSet」を得る。
+		//(12)executeQueryを実行し、結果の「ResultSet」を得る。
 		rs = pstmt.executeQuery();
 
-		//(13)linkedhashmapを宣言する。
+		//(13)linkedhashmapを宣言する。これはコメントのデータを格納するMapとなる。
+		//keyはコメントId、データはParentBean型とする。
 		LinkedHashMap<String,ParentBean> parentMap = new LinkedHashMap<String, ParentBean>();
 
-		//(13)while文を通してlinkedhashmapにコメントタイプ0のデータを入力する。
+		//(13)while文を通してlinkedhashmapにコメントタイプ0、通常のコメントに関するデータを入力する。
 		while(rs.next()) {
 			ParentBean parBean = new ParentBean();
 			parBean.setComment(rs.getString("comment"));
@@ -206,30 +207,44 @@ public class S00003 extends HttpServlet{
 			parentMap.put(commentId , parBean);
 		}
 
-		//(14)続いてコメントタイプ1（返信コメントのデータ）をSql文を使って取得する。手順は（10）～（12）と同様だが、二つ目の?には1を充てる。
+		//(14)続いてコメントタイプ1（返信コメントのデータ）をSql文を使って取得する。
+		//手順は（10）～（12）と同様だが、二つ目の?には1を充てる。
 		pstmt = con.prepareStatement(commentSql);
 		pstmt.setString(1, id);
+
+		//ここの？に、コメントタイプ1のデータをとるため"1"を代入する。
 		pstmt.setString(2, "1");
 		rs = pstmt.executeQuery();
 
-		//(15)(13)でlinkedhashmapに追加したデータの中で、今回取得したtoCommentIdの値が等しいキーがある場合、そのデータのlistにcommentBeanをaddする。
+		//(15)(13)でlinkedhashmapに追加したデータの中で、今回取得したtoCommentIdの値が等しいキーがある場合、
+		//SQLでコメントに関するデータを取得する。
 		while(rs.next()) {
-			CommentBean comBean = new CommentBean();
-			comBean.setComment(rs.getString("comment"));
-			comBean.setComposerId(CommonUtils.idformat(rs.getLong("composer_id")));
-			comBean.setType(rs.getString("type"));
-			comBean.setWriteDatetime(CommonUtils.epoch(rs.getDouble("write_datetime")));
-			comBean.setRating(CommonUtils.starformat(CommonUtils.ratingformat(rs.getByte("rating"))));
-			comBean.setNickname2(rs.getString("nickname"));
-			comBean.setUniqueCode2(rs.getString("unique_code"));
 			String toCommentId =CommonUtils.idformat(rs.getLong("to_comment_id"));
-			ParentBean data = parentMap.get(toCommentId);
-			data.getreplyComment().add(comBean);
+			if(parentMap.containsKey(toCommentId)) {
+				CommentBean comBean = new CommentBean();
+				comBean.setComment(rs.getString("comment"));
+				comBean.setComposerId(CommonUtils.idformat(rs.getLong("composer_id")));
+				comBean.setType(rs.getString("type"));
+				comBean.setWriteDatetime(CommonUtils.epoch(rs.getDouble("write_datetime")));
+				comBean.setRating(CommonUtils.starformat(CommonUtils.ratingformat(rs.getByte("rating"))));
+				comBean.setNickname2(rs.getString("nickname"));
+				comBean.setUniqueCode2(rs.getString("unique_code"));
+				ParentBean data = parentMap.get(toCommentId);
+
+				//toCommentIdに該当するキーを持つvalueが持っているreplyCommentListにcomBeanの値を格納する。
+				data.getreplyComment().add(comBean);
+			}
 		}
 
+		//(16)出力時の処理を軽減するため、parentMapの値をcommentListに格納しなおす作業を行う。
 		List<CommentBean> commentList = new ArrayList<CommentBean>();
 		for (Iterator<ParentBean> itr = parentMap.values().iterator(); itr.hasNext();) {
+
+			//mapにデータがある限り、ParentBean型の変数pbにそのデータを入れ込む。
 			ParentBean pb = itr.next();
+
+			//CommentBean型のcbというインスタンスを宣言し、それらにpbの中にあるコメントに関するデータを格納する。
+			//その後cbをcommentListに格納する。
 			CommentBean cb = new CommentBean();
 			cb.setComment(pb.getComment());
 			cb.setSequence(pb.getSequence());
@@ -240,6 +255,9 @@ public class S00003 extends HttpServlet{
 			cb.setUniqueCode2(pb.getUniqueCode2());
 			cb.setRating(pb.getRating());
 			commentList.add(cb);
+
+			//mapから得たvalueの中に(15)で値を格納したListがある場合、そのListのデータをcommentListにaddしていく。
+			//これはvalueの中のlistが存在する限り繰り返す。
 			if (0 < pb.getreplyComment().size()) {
 				for( CommentBean replycb : pb.getreplyComment() ) {
 					commentList.add(replycb);
@@ -247,42 +265,37 @@ public class S00003 extends HttpServlet{
 			}
 		}
 
-		//（14）以上でCommentListに追加したデータをsetAttributeする。
+		//(17)以上でcommentListに追加したデータをsetAttributeする。
 		request.setAttribute("commentList", commentList);
-		
-		//（15）jsp画像が押下された際にmelokoを起動するURLを作成する。
+
+		//(18)jsp画像が押下された際にmelokoを起動するURLを作成する。
 		String melokoUrl = "meloko://?song_id=".concat(id);
 
-		//（16）（15）で設定したmelokoUrlをsetAttributeする。
+		//(19)(15)で設定したmelokoUrlをsetAttributeする。
 		request.setAttribute("melokoUrl", melokoUrl);
 
-		//（17）RatingSqlを使い、引数で渡されたcon.prepareStatement()メソッドを実行する。
+		//(20)RatingSqlを使い、引数で渡されたcon.prepareStatement()メソッドを実行する。
 		pstmt = con.prepareStatement(ratingSql);
 
-		//(18)Sqlの？に（3）で値を代入したidをセットして実行する
+		//(21)Sqlの？に（3）で値を代入したidをセットして実行する
 		pstmt.setString(1, id);
 
-		//（19）executeQueryを実行し、結果の「ResultSet」を得る。
+		//(22)executeQueryを実行し、結果の「ResultSet」を得る。
 		rs = pstmt.executeQuery();
 
-		//(20)RatingSqlでのデータを受け取る変数を宣言する。
+		//(23)RatingSqlでのデータを受け取る変数を宣言する。
 		String ratingAverage = null;
 		String ratingTotal= null;
 
+		//(24)それぞれの値を（20）で宣言した変数に追加。
+		ratingAverage = CommonUtils.averageformat(rs.getDouble("rating_average"));
+		ratingTotal = CommonUtils.valueformat(rs.getLong("rating_total"));
 
-		//(21)while文を通してそれぞれの値を（20）で宣言した変数にに追加。
-		while(rs.next()) {
-			ratingAverage = CommonUtils.averageformat(rs.getDouble("rating_average"));
-			ratingTotal = CommonUtils.valueformat(rs.getLong("rating_total"));
-		}
-
-		//（22）取得した値（変数）をsetAttributeする。
+		//（25）取得した値（変数）をsetAttributeする。
 		request.setAttribute("rating_average", ratingAverage);
 		request.setAttribute("rating_total", ratingTotal);
 
-		//（23）S00003にフォワーディングする。
+		//（26）S00003にフォワーディングする。
 		getServletConfig().getServletContext().getRequestDispatcher("/jsp/S00003.jsp" ).forward( request, response );
-
-
 	}
 }
