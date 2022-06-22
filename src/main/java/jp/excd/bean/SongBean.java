074cbf53b986339ec720f78d9f93b7e5b955e24d
m.noguchi <masahiro.noguchi@excd.jp>
2022-06-22 17:57:55 +0900

    マージ

--- src/main/java/jp/excd/bean/SongBean.java
+++ src/main/java/jp/excd/bean/SongBean.java
@@ -3,22 +3,66 @@ package jp.excd.bean;
 public class SongBean {
 
 	private String title;
+	private String ratingTotal = null;
+	private String ratingAverage = null;
+	private String totalListenCount = null;
+	private String releaseDatetime = null;
+	private String lastUpdateDatetime = null;
 	private String rating_total_formated;
 	private String rating_average_formated;
 	private String total_listen_count_formated;
 	private String release_datetime_formated;
 	private String image_file_name;
-	private int	imageFileHeight;
-	private int	imageFileWidth;
+	private String imageFileName = null;
+	private double imageFileHeight;
+	private double imageFileWidth;
 	private double formatHeight;
 	private double cutLength;
 	private String song_id;
+	private String message = null;
+	private String key = null;
+	private String scoreType = null;
+	private String bpm = null;
+	private String otherLinkUrl = null;
+	private String uniqueCode1 = null;
+	private String otherLinkDescription = null;
+	private String nickname1 = null;
 
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	public String getRatingTotal() {
+		return ratingTotal;
+	}
+	public void setRatingTotal(String ratingTotal) {
+		this.ratingTotal = ratingTotal;
+	}
+	public String getRatingAverage() {
+		return ratingAverage;
+	}
+	public void setRatingAverage(String ratingAverage) {
+		this.ratingAverage = ratingAverage;
+	}
+	public String getTotalListenCount() {
+		return totalListenCount;
+	}
+	public void setTotalListenCount(String totalListenCount) {
+		this.totalListenCount = totalListenCount;
+	}
+	public String getReleaseDatetime() {
+		return releaseDatetime;
+	}
+	public void setReleaseDatetime(String releaseDatetime) {
+		this.releaseDatetime = releaseDatetime;
+	}
+	public String getLastUpdateDatetime() {
+		return lastUpdateDatetime;
+	}
+	public void setLastUpdateDatetime(String lastUpdateDatetime) {
+		this.lastUpdateDatetime = lastUpdateDatetime;
 	}
 	public String getRating_total_formated() {
 		return rating_total_formated;
@@ -50,16 +94,22 @@ public class SongBean {
 	public void setImage_file_name(String image_file_name) {
 		this.image_file_name = image_file_name;
 	}
-	public int getImageFileHeight() {
+	public String getImageFileName() {
+		return imageFileName;
+	}
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+	public double getImageFileHeight() {
 		return imageFileHeight;
 	}
-	public void setImageFileHeight(int imageFileHeight) {
+	public void setImageFileHeight(double imageFileHeight) {
 		this.imageFileHeight = imageFileHeight;
 	}
-	public int getImageFileWidth() {
+	public double getImageFileWidth() {
 		return imageFileWidth;
 	}
-	public void setImageFileWidth(int imageFileWidth) {
+	public void setImageFileWidth(double imageFileWidth) {
 		this.imageFileWidth = imageFileWidth;
 	}
 	public double getFormatHeight() {
@@ -80,5 +130,52 @@ public class SongBean {
 	public void setSong_id(String song_id) {
 		this.song_id = song_id;
 	}
-
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getScoreType() {
+		return scoreType;
+	}
+	public void setScoreType(String scoreType) {
+		this.scoreType = scoreType;
+	}
+	public String getBpm() {
+		return bpm;
+	}
+	public void setBpm(String bpm) {
+		this.bpm = bpm;
+	}
+	public String getOtherLinkUrl() {
+		return otherLinkUrl;
+	}
+	public void setOtherLinkUrl(String otherLinkUrl) {
+		this.otherLinkUrl = otherLinkUrl;
+	}
+	public String getUniqueCode1() {
+		return uniqueCode1;
+	}
+	public void setUniqueCode1(String uniqueCode1) {
+		this.uniqueCode1 = uniqueCode1;
+	}
+	public String getOtherLinkDescription() {
+		return otherLinkDescription;
+	}
+	public void setOtherLinkDescription(String otherLinkDescription) {
+		this.otherLinkDescription = otherLinkDescription;
+	}
+	public String getNickname1() {
+		return nickname1;
+	}
+	public void setNickname1(String nickname1) {
+		this.nickname1 = nickname1;
+	}
 }
