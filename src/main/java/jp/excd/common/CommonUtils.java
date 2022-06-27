5985057df9d6bade96dd7207d88839221bb5a95d
n-nishiza <natsumi.nishiza@excd.jp>
2022-06-27 18:53:13 +0900

    マージ

--- src/main/java/jp/excd/common/CommonUtils.java
+++ src/main/java/jp/excd/common/CommonUtils.java
@@ -78,6 +78,8 @@ public class CommonUtils {
 	public static String averageformat(Double a) {
 		
 		DecimalFormat df = new DecimalFormat("#.#");
+		df.setMinimumFractionDigits(1);
+		df.setMaximumFractionDigits(1);
 		String average = df.format(a);
 		return average;
 	}
@@ -227,18 +229,178 @@ public class CommonUtils {
 	}
 
 	/**
-	 * 入力値の空欄チェック（空欄 or Null）を行う
-	 * @param value チェック対象値
-	 * @return True:空欄 or Null,  False:それ以外
+	 * 評価のString型変換
+	 * @param byte型
+	 * @return String型 
+	 * 	 */
+	
+	public static String ratingformat(byte a) {
+		return String.valueOf(a);
+	}
+
+	/**
+	 * IDのString型変換
+	 * @param long型
+	 * @return String型 
+	 * 	 */
+	
+	public static String idformat(long a2) {
+		return String.valueOf(a2);
+	}
+
+	/**
+	 * キーの日本語表記
+	 * @param 数字（テーブル設計書最後のsheetにあるキー変換）
+	 * @return 変換後(String型の日本語表記キー)
 	 */
-	public static  boolean isBlank(String value) {
-		if (value == null || "".equals(value)) {
-			return true;
-		} else {
-			return false;
+	public static String keyName(String a2) {
+		if("1".equals(a2)) {
+			a2 = "Cメジャー";
+		}else if("2".equals(a2)) {
+			a2 = "Cシャープメジャー";
+		}else if("3".equals(a2)) {
+			a2 = "Dフラットメジャー";
+		}else if("4".equals(a2)) {
+			a2 = "Dメジャー";
+		}else if("5".equals(a2)) {
+			a2 = "Dシャープメジャー";
+		}else if("6".equals(a2)) {
+			a2 = "Eフラットメジャー";
+		}else if("7".equals(a2)) {
+			a2 = "Eメジャー";
+		}else if("8".equals(a2)) {
+			a2 = "Fメジャー";
+		}else if("9".equals(a2)) {
+			a2 = "Fシャープメジャー";
+		}else if("10".equals(a2)) {
+			a2 = "Gフラットメジャー";
+		}else if("11".equals(a2)) {
+			a2 = "Gメジャー";
+		}else if("12".equals(a2)) {
+			a2 = "Gシャープメジャー";
+		}else if("13".equals(a2)) {
+			a2 = "Aフラットメジャー";
+		}else if("14".equals(a2)) {
+			a2 = "Aメジャー";
+		}else if("15".equals(a2)) {
+			a2 = "Aシャープメジャー";
+		}else if("16".equals(a2)) {
+			a2 = "Bフラットメジャー";
+		}else if("17".equals(a2)) {
+			a2 = "Bメジャー";
+		}else if("18".equals(a2)) {
+			a2 = "Cマイナー";
+		}else if("19".equals(a2)) {
+			a2 = "Cシャープマイナー";
+		}else if("20".equals(a2)) {
+			a2 = "Dフラットマイナー";
+		}else if("21".equals(a2)) {
+			a2 = "Dマイナー";
+		}else if("22".equals(a2)) {
+			a2 = "Dシャープマイナー";
+		}else if("23".equals(a2)) {
+			a2 = "Eフラットマイナー";
+		}else if("24".equals(a2)) {
+			a2 = "Eマイナー";
+		}else if("25".equals(a2)) {
+			a2 = "Fマイナー";
+		}else if("26".equals(a2)) {
+			a2 = "Fシャープマイナー";
+		}else if("27".equals(a2)) {
+			a2 = "Gフラットマイナー";
+		}else if("28".equals(a2)) {
+			a2 = "Gマイナー";
+		}else if("29".equals(a2)) {
+			a2 = "Gシャープマイナー";
+		}else if("30".equals(a2)) {
+			a2 = "Aフラットマイナー";
+		}else if("31".equals(a2)) {
+			a2 = "Aマイナー";
+		}else if("32".equals(a2)) {
+			a2 = "Aシャープマイナー";
+		}else if("33".equals(a2)) {
+			a2 = "Bフラットマイナー";
+		}else if("34".equals(a2)) {
+			a2 = "Bマイナー";
 		}
+		return a2;
+
+	}
+
+	/**
+	 * 楽譜表記のフォーマット化
+	 * @param String型の0、または1。
+	 * @return テーブル設計書内で指定された表記方法
+	 */
+	
+	public static String scoreChange(String a3) {
+		
+		if("0".equals(a3)) {
+			a3 = "楽譜通り";
+		}else if("1".equals(a3)) {
+			a3 = "1オクターブ上で表記";
+		}
+		
+		return a3;
+	}
+		
+	/**
+	 * 評価のフォーマット。ratingによってhmtlのクラスを変更する。
+	 * @param 評価数値（rating）
+	 * @return 評価に応じたクラス名
+	 * */
+
+	public static String starformat(String str5) {
+		
+		String rating =null;
+		
+		if ("5" .equals (str5)) {
+			rating = "rating star50";
+		}else if("4.5" .equals (str5)){
+			rating = "rating star45";
+		}else if("4" .equals (str5)){
+			rating = "rating star40";
+		}else if("3.5" .equals (str5)){
+			rating = "rating star35";
+		}else if("3" .equals (str5)){
+			rating = "rating star30";
+		}else if("2.5" .equals (str5)){
+			rating = "rating star25";
+		}else if("2" .equals (str5)){
+			rating = "rating star20";
+		}else if("1.5" .equals (str5)){
+			rating = "rating star15";
+		}else if("1" .equals (str5)){
+			rating = "rating star10";
+		}else if("0.5" .equals (str5)){
+			rating = "rating star05";
+		}else if("0" .equals (str5)){
+			rating = "rating star00";
+		}
+		
+		return rating;
 	}
 	
+	public static double imageHeightformat(int width , int height) {
+		double formatHeight = height; //ダブル型に変換
+		double width1 =width; //ダブル型に変換
+		if(width != 275) { //横幅が275px固定なので、もし違う場合は拡大・縮小しなければならない
+			double widthRate= 275/width1; //この計算で高さを何倍させれば良いか出す
+			formatHeight = (widthRate*formatHeight); //調整後の高さ
+		}
+		
+		return formatHeight;
+		
+	}
+
+	public static double cutLength(double d1) {
+		double imageLength = 0;
+		if(d1 != 160) { //高さを160にしなければならない
+			imageLength = (d1-160)/2; //高さから160を引き、それを2で割ると、画像をどれだけカットすれば良いか分かる
+		}
+		return imageLength;
+	}
+
 	/**
 	 * 入力値の整数チェックを行う
 	 * @param value チェック対象値
